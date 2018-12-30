830eb38f5bb14ba625d9560f4105051403b538ff
hwlee8042 <hwlee8042@naver.com>
2018-12-30 23:33:45 +0900

    JavaStudy_181230

--- chapter07/ch7_181230/PointTest.java
+++ chapter07/ch7_181230/PointTest.java
@@ -7,7 +7,10 @@ Super() : 조상클래스의 생성자를 호출하는데 사용된다.
 */
 class PointTest {
     public static void main(String[] args) {
-        Point3D p3 = new Point3D();
+        Point3D p3 = new Point3D(1, 2, 3);
+        System.out.println("p3.x = " + p3.x);
+        System.out.println("p3.y = " + p3.y);
+        System.out.println("p3.z = " + p3.z);
     }
 }
 
